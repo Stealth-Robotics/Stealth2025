d7c238b2e935716aa4633563b85ca6de271eaaad
oliverstivers <oliverstivers@gmail.com>
2025-01-14 08:41:32 -0800

    formatting

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,14 +8,15 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 
 public class RobotContainer {
-  public RobotContainer() {
-    Superstructure superstructure = new Superstructure();
-    configureBindings();
-  }
+	public RobotContainer() {
+		Superstructure superstructure = new Superstructure();
+		configureBindings();
+	}
 
-  private void configureBindings() {}
+	private void configureBindings() {
+	}
 
-  public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
-  }
+	public Command getAutonomousCommand() {
+		return Commands.print("No autonomous command configured");
+	}
 }

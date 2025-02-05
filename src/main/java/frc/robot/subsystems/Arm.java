aeac767be8efb372a0dfcb367da6d1eef6304d28
Ian N <littlepenguin24@outlook.com>
2025-02-05 11:59:19 -0800

    Updated to fulfill Oliver's Requests

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -6,16 +6,21 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 
+@Logged
 public class Arm extends SubsystemBase{
     
     private final double ARM_GEAR_RATIO = 1;
     private final TalonFX armMotor;
     
-    private final double ZeroOffset = 0;
+    private final double ZERO_OFFSET = 0;
+    private final double DEGREES_TO_TICKS = 360;
+
+    private double armTargetPosition = 0; //Target position as a variable for logging purposes
 
     private final double kP = 0;
     private final double kI = 0;
@@ -52,7 +57,7 @@ public class Arm extends SubsystemBase{
         armMotor.getConfigurator().apply(armMotorConfiguration);
     }
     private void resetEncoder(){
-        armMotor.setPosition(ZeroOffset);
+        armMotor.setPosition(ZERO_OFFSET);
     }
     private double getMotorPosition(){
         return armMotor.getPosition().getValueAsDouble();
@@ -60,19 +65,17 @@ public class Arm extends SubsystemBase{
     private double getTargetPosition(){
         return motionMagicVoltage.Position;
     }
-    private boolean isMotorAtTarget(double tolerance){
-        return Math.abs(getMotorPosition()-getTargetPosition()) <= tolerance;
+    private boolean isMotorAtTarget(){
+        return Math.abs(getMotorPosition()-getTargetPosition()) <= kTolerance;
     }
     private void setTargetPosition(double degrees){
         motionMagicVoltage.Position = degrees;
+        armTargetPosition = degrees;
         armMotor.setControl(motionMagicVoltage);
     }
-    public Command rotateToPositionCommand(DoubleSupplier degrees, double tolerance){
-        return this.runOnce(()->setTargetPosition(degrees.getAsDouble()))
-        .andThen(new WaitUntilCommand(()->this.isMotorAtTarget(tolerance)));
-    }
-    public Command rotateWhileDrivingCommand(DoubleSupplier degrees){
-        return this.runOnce(()->setTargetPosition(degrees.getAsDouble()));
+    public Command rotateToPositionCommand(DoubleSupplier degrees){
+        return this.runOnce(()->setTargetPosition(degrees.getAsDouble()*DEGREES_TO_TICKS))
+        .andThen(new WaitUntilCommand(()->this.isMotorAtTarget()));
     }
 
     
